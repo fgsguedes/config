b5b91e68994018a2b911555284047e138ff33202
Guedes <fgs.guedes@gmail.com>
2026-06-20 23:34:19 +0200

    nvim - Update default tab size and re-ident nvim lua files

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,10 @@
 vim.opt.guicursor = "i:hor20"
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
 
 require("config.lazy")
 

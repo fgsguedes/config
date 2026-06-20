36f12ff3d0dac1fc99cf405e33e06bc273216294
Guedes <fgs.guedes@gmail.com>
2026-06-21 00:03:34 +0200

    nvim - set softtabstop

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,7 @@ vim.opt.guicursor = "i:hor20"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
